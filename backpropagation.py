5e81182adb81e46858c7cd2e8310896f6a3ae620
Kis <linh.nt.iot@gmail.com>
2020-04-27 23:01:58 +0700

    add backpro

--- backpropagation.py
+++ backpropagation.py
@@ -27,11 +27,13 @@ cur_axes.axes.get_xaxis().set_ticks([])
 cur_axes.axes.get_yaxis().set_ticks([])
 
 plt.savefig('EX.png', bbox_inches='tight', dpi=600)
-# plt.show()
+plt.show()
+
+
 def softmax(V):
-  e_V = np.exp(V - np.max(V, axis=0, keepdims=True))
-  Z = e_V / e_V.sum(axis=0)
-  return Z
+    e_V = np.exp(V - np.max(V, axis=0, keepdims=True))
+    Z = e_V / e_V.sum(axis=0)
+    return Z
 
 
 ## One-hot coding
@@ -39,27 +41,28 @@ from scipy import sparse
 
 
 def convert_labels(y, C=3):
-  Y = sparse.coo_matrix((np.ones_like(y),
-                         (y, np.arange(len(y)))), shape=(C, len(y))).toarray()
-  return Y
+    Y = sparse.coo_matrix((np.ones_like(y),
+                           (y, np.arange(len(y)))), shape=(C, len(y))).toarray()
+    return Y
 
 
 # cost or loss function
 def cost(Y, Yhat):
-  return -np.sum(Y * np.log(Yhat)) / Y.shape[1]
+    return -np.sum(Y * np.log(Yhat)) / Y.shape[1]
+
 
 d0 = 2
-d1 = h = 100 # size of hidden layer
+d1 = h = 100  # size of hidden layer
 d2 = C = 3
 # initialize parameters randomly
-W1 = 0.01*np.random.randn(d0, d1)
+W1 = 0.01 * np.random.randn(d0, d1)
 b1 = np.zeros((d1, 1))
-W2 = 0.01*np.random.randn(d1, d2)
+W2 = 0.01 * np.random.randn(d1, d2)
 b2 = np.zeros((d2, 1))
 
 Y = convert_labels(y, C)
 N = X.shape[1]
-eta = 1 # learning rate
+eta = 1  # learning rate
 for i in range(10000):
     ## Feedforward
     Z1 = np.dot(W1.T, X) + b1
@@ -68,28 +71,28 @@ for i in range(10000):
     Yhat = softmax(Z2)
 
     # print loss after each 1000 iterations
-    if i %1000 == 0:
+    if i % 1000 == 0:
         # compute the loss: average cross-entropy loss
         loss = cost(Y, Yhat)
-        print("iter %d, loss: %f" %(i, loss))
+        print("iter %d, loss: %f" % (i, loss))
 
     # backpropagation
-    E2 = (Yhat - Y )/N
+    E2 = (Yhat - Y) / N
     dW2 = np.dot(A1, E2.T)
-    db2 = np.sum(E2, axis = 1, keepdims = True)
+    db2 = np.sum(E2, axis=1, keepdims=True)
     E1 = np.dot(W2, E2)
-    E1[Z1 <= 0] = 0 # gradient of ReLU
+    E1[Z1 <= 0] = 0  # gradient of ReLU
     dW1 = np.dot(X, E1.T)
-    db1 = np.sum(E1, axis = 1, keepdims = True)
+    db1 = np.sum(E1, axis=1, keepdims=True)
 
     # Gradient Descent update
-    W1 += -eta*dW1
-    b1 += -eta*db1
-    W2 += -eta*dW2
-    b2 += -eta*db2
+    W1 += -eta * dW1
+    b1 += -eta * db1
+    W2 += -eta * dW2
+    b2 += -eta * db2
 
 Z1 = np.dot(W1.T, X) + b1
 A1 = np.maximum(Z1, 0)
 Z2 = np.dot(W2.T, A1) + b2
 predicted_class = np.argmax(Z2, axis=0)
-print('training accuracy: %.2f %%' % (100*np.mean(predicted_class == y)))
+print('training accuracy: %.2f %%' % (100 * np.mean(predicted_class == y)))
